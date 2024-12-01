c70bb3d4471043755ca81296035c118d99eab601
arctlite <arctlite@outlook.com>
2024-12-01 07:45:34 +0700

    fix signing

--- scripts/pojav/sign_apks.sh
+++ scripts/pojav/sign_apks.sh
@@ -7,8 +7,7 @@ ANDROID_TOOLS_PATH="$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/build-t
 export PATH="$ANDROID_TOOLS_PATH:$PATH"
 
 # Align APKs
-zipalign -v -p 4 "$APK_PATH/app_pojavlauncher-armeabi-v7a-release-unsigned.apk" "$APK_PATH/app_pojavlauncher-armeabi-v7a-aligned.apk"
-zipalign -v -p 4 "$APK_PATH/app_pojavlauncher-arm64-v8a-release-unsigned.apk" "$APK_PATH/app_pojavlauncher-arm64-v8a-aligned.apk"
+zipalign -v -p 4 "$APK_PATH/app_pojavlauncher-release-unsigned.apk" "$APK_PATH/app_pojavlauncher-aligned.apk"
 
 # Decode signing key
 echo "$SIGNING_KEY" | base64 -d > signing_key.jks
@@ -16,15 +15,9 @@ echo "$SIGNING_KEY" | base64 -d > signing_key.jks
 # Sign APKs
 apksigner sign --ks signing_key.jks --ks-key-alias "$KEY_ALIAS" \
                --ks-pass pass:"$SIGNING_KEY_PASSWORD" --key-pass pass:"$KEY_PASSWORD" \
-               --out "$APK_PATH/app_pojavlauncher-armeabi-v7a-signed.apk" \
-               "$APK_PATH/app_pojavlauncher-armeabi-v7a-aligned.apk"
-
-apksigner sign --ks signing_key.jks --ks-key-alias "$KEY_ALIAS" \
-               --ks-pass pass:"$SIGNING_KEY_PASSWORD" --key-pass pass:"$KEY_PASSWORD" \
-               --out "$APK_PATH/app_pojavlauncher-arm64-v8a-signed.apk" \
-               "$APK_PATH/app_pojavlauncher-arm64-v8a-aligned.apk"
+               --out "$APK_PATH/app_pojavlauncher-signed.apk" \
+               "$APK_PATH/app_pojavlauncher-aligned.apk"
 
 # Verify signed APKs
-apksigner verify "$APK_PATH/app_pojavlauncher-armeabi-v7a-signed.apk"
-apksigner verify "$APK_PATH/app_pojavlauncher-arm64-v8a-signed.apk"
+apksigner verify "$APK_PATH/app_pojavlauncher-signed.apk"
 
